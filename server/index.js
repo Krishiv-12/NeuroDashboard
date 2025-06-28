d5aaa7ac3f48b6ce6caedb75fd2d6af86740f462
Krishiv <golusinghrawat712@gmail.com>
2025-06-28 07:25:55 +0530

    changes for deployment

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+
+dotenv.config()
+
 import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
 import { protect } from './middleware/authMiddleware.js'
 import taskRoutes from "./routes/taskRoutes.js";
-import openaiRoutes from "./routes/groqRoutes.js";
-
+import openaiRoutes from "./routes/groqRoutes.js"
 connectDB()
 
-dotenv.config()
 const app = express()
 
 app.use(cors())
@@ -20,12 +21,17 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/protected', protect, (req, res) => {
-    res.json({ message: `Hello ${req.user.id}, you accessed a protected route!` })
-  })
+  res.json({ message: `Hello ${req.user.id}, you accessed a protected route!` })
+})
 
 app.use('/api/auth', authRoutes)
-app.use("/api/tasks", taskRoutes);
-app.use("/api/openai", openaiRoutes);
+app.use("/api/tasks", taskRoutes)
+app.use("/api/openai", openaiRoutes)
 
 const PORT = process.env.PORT || 5001
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`))
+
+
+// TdTWydKTZEBwi5Gn
+
+// mongodb+srv://golusinghrawat712:TdTWydKTZEBwi5Gn@neurodashboard.1ku0zln.mongodb.net/
